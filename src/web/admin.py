8044e1e913b39950672874b759b498e373646a6e
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-04-24 17:22:06 +0300

    admin update

--- src/web/admin.py
+++ src/web/admin.py
@@ -3,12 +3,41 @@ from django.contrib import admin
 from .models import Block, Skill, TeamMember
 
 
+class TeamMemberInline(admin.TabularInline):
+    model = TeamMember
+    extra = 1
+
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 1
+
 
 class BlockAdmin(admin.ModelAdmin):
     model = Block
 
     list_display = ('name', 'created_at', 'position', 'in_menu')
-    list_filter = ('name', 'created_at', )
+    list_filter = ('name', 'created_at')
+
+    inlines = []
+    skill_inline = [SkillInline, ]
+    team_inline = [TeamMemberInline, ]
+
+
+    def get_inline_instances(self, request, obj=None):
+        inline_instances = []
+
+        if obj.skill_list.all():
+            inlines = self.skill_inline
+        elif obj.team_list.all():
+            inlines = self.team_inline
+        else:
+            inlines = self.inlines
+
+        for inline_class in inlines:
+            inline = inline_class(self.model, self.admin_site)
+            inline_instances.append(inline)
+        return inline_instances
 
 
 admin.site.register(Block, BlockAdmin)
